d3d48602fd32a85622692b709036b58779c786ca
SANGCO <nsg1213@gmail.com>
2020-03-20 09:37:28 +0900

    프로그래머스: 전화번호 목록

---
+++ src/main/java/com/example/programmers/해시/전화번호_목록.java
@@ -0,0 +1,18 @@
+package com.example.programmers.해시;
+
+public class 전화번호_목록 {
+    public static boolean solution(String[] phoneBook) {
+        for(int i = 0; i < phoneBook.length - 1; i++) {
+            for(int j = i + 1; j < phoneBook.length; j++) {
+                if(phoneBook[i].startsWith(phoneBook[j])) return false;
+                if(phoneBook[j].startsWith(phoneBook[i])) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Boolean answer = solution(new String[]{"123", "456", "789"});
+        System.out.println(answer);
+    }
+}
