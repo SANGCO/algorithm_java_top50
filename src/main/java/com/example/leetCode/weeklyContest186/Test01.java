59a98de92fea402f13706b0de205c22f51dd28da
SANGCO <nsg1213@gmail.com>
2020-04-27 08:50:31 +0900

    리트코드 코딩 테스트 2020-04-26 다른 사람답

--- src/main/java/com/example/leetCode/weeklyContest186/Test01.java
+++ src/main/java/com/example/leetCode/weeklyContest186/Test01.java
@@ -63,4 +63,20 @@ public class Test01 {
         return answer;
     }
 
+    public int maxScore1(String s) {
+        int n = s.length();
+        int c1 = 0;
+        for(int i = 0; i < n; i++) {
+            if(s.charAt(i) == '1') c1++;
+        }
+        int max = 0;
+        int c2 = 0;
+        for(int i = 0; i < n - 1; i++) {
+            if(s.charAt(i) == '0') c2++;
+            else c1--;
+            max = Math.max(max, c1 + c2);
+        }
+        return max;
+    }
+
 }
--- src/main/java/com/example/leetCode/weeklyContest186/Test02.java
+++ src/main/java/com/example/leetCode/weeklyContest186/Test02.java
@@ -20,5 +20,21 @@ public class Test02 {
         answer = Math.max(temp1, temp2);
         return answer;
     }
+    public int maxScore1(int[] cardPoints, int k) {
+        int res = 0;
+        int len = cardPoints.length;
+
+        int start = len - k;
+        int win = 0;
+
+        for (int i = start; i < len + k; ++i) {
+            win += cardPoints[i % len]; // accumulate the value of the sliding window.
+            if (i - start >= k) { // Is the sliding window wider than k?
+                win -= cardPoints[(i - k) % len]; // deduct the element from the left of the window.
+            }
+            res = Math.max(win, res); // update the maximum.
+        }
+        return res;
+    }
 
 }
