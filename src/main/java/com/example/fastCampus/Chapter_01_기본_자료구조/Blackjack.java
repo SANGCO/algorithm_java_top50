d29237b27ff978341794499fb394c8a116300970
SANGCO <nsg1213@gmail.com>
2020-05-04 19:04:39 +0900

    패스트 캠퍼스 : Blackjack

---
+++ src/main/java/com/example/fastCampus/Chapter_01_기본_자료구조/Blackjack.java
@@ -0,0 +1,33 @@
+package com.example.fastCampus.Chapter_01_기본_자료구조;
+
+import java.util.Scanner;
+
+public class Blackjack {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, m;
+        int[] data = new int[100];
+        int result = 0;
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            data[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                for (int k = j + 1; k < n; k++) {
+                    int sum = data[i] + data[j] + data[k];
+                    if (sum <= m) result = Math.max(result, sum);
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+}
