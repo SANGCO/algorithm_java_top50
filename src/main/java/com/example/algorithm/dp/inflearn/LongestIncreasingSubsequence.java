b78c444193158635ca41e4d747af04abeda162b9
SANGCO <nsg1213@gmail.com>
2020-08-29 23:53:31 +0900

    LongestIncreasingSubsequence

--- src/main/java/com/example/algorithm/dp/inflearn/LongestIncreasingSubsequence.java
+++ src/main/java/com/example/algorithm/dp/inflearn/LongestIncreasingSubsequence.java
@@ -8,25 +8,27 @@ public class LongestIncreasingSubsequence {
 
         LongestIncreasingSubsequence a = new LongestIncreasingSubsequence();
         int[] nums = {1, 2, 3, 2, 5, 2, 6, 10, 4, 12};
-//		int[] nums = {9, 11, 2, 8, 4, 7, 88, 15};
+//		int[] nums = {9,11,2,8,4,7,88,15};
 
-        System.out.println("value "+ solve(nums));
+        System.out.println("value " + a.solve(nums));
     }
 
-    private static int solve(int[] nums) {
+    public int solve(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int result = 1;
 
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1;
-
+        for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
                 }
             }
+            result = Math.max(result, dp[i]);
         }
-
-        return Arrays.stream(dp).max().getAsInt();
+        return result;
     }
-
 }
